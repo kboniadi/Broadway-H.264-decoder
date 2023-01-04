6b05ec1f2985c226d8505cd3d243859f700a8649
Kord Boniadi <kordbon1@gmail.com>
2023-01-04 14:12:44 -0800

    demo template made

--- Decoder/src/main.c
+++ Decoder/src/main.c
@@ -1,14 +1,16 @@
 #include "Decoder.h"
 
 
-void decode(typedAr, parInfo) {
-    
+void decode(Decoder *dec, u8* stream , u8* typedAr, int length) {
+    memcpy(stream, typedAr, length);
+    broadwayPlayStream(dec, length);
 }
+
 int main() {
     Decoder dec;
 
     broadwayInit(&dec);
     u8* stream = broadwayCreateStream(&dec, 1024 * 1024);
 
-
+    broadwayExit(&dec);
 }
