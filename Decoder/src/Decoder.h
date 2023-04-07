7e105b92599e33af593c9dcb0fd8506bb695a8d7
Kord Boniadi <kordbon1@gmail.com>
2023-04-07 16:58:36 -0700

    update decoder.h

--- Decoder/src/Decoder.h
+++ Decoder/src/Decoder.h
@@ -1,54 +1,56 @@
 #ifndef DECODER_H_
 #define DECODER_H_
-
-#include "H264SwDecApi.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "H264SwDecApi.h"
+
 /* Debug prints */
 #define DEBUG(argv) printf argv
 
-#include "opttarget.h"
-
 #include "extraFlags.h"
+#include "opttarget.h"
 
 #define STREAM_BUFFER_SIZE = 1024 * 1024;
 /* Debug prints */
 #define DEBUG(argv) printf argv
 
 typedef struct {
-    u32 length;
-    u8 *buffer;
-    u8 *pos;
-    u8 *end;
+	u32 length;
+	u8 *buffer;
+	u8 *pos;
+	u8 *end;
 } Stream;
 
 typedef struct {
-    /*options*/
-    int nalUnitStream;
-    int packetize;
-    int cropDisplay;
-    u32 disableOutputReordering;
+	/*options*/
+	int nalUnitStream;
+	int packetize;
+	int cropDisplay;
+	u32 disableOutputReordering;
 
-    u8 *streamStop;
-    u8 *tmpImage;
+	u8 *streamStop;
+	u8 *tmpImage;
 
-    u8 *streamBuffer;
+	u8 *streamBuffer;
 
-    H264SwDecInst decInst;
-    H264SwDecInput decInput;
-    H264SwDecOutput decOutput;
-    H264SwDecPicture decPicture;
-    H264SwDecInfo decInfo;
+	H264SwDecInst decInst;
+	H264SwDecInput decInput;
+	H264SwDecOutput decOutput;
+	H264SwDecPicture decPicture;
+	H264SwDecInfo decInfo;
 
-    u32 picDecodeNumber;
-    u32 picDisplayNumber;
-    u32 picSize;
+	u32 picDecodeNumber;
+	u32 picDisplayNumber;
+	u32 picSize;
 
-    Stream stream;
-    Stream broadwayStream;
+	Stream stream;
+	Stream broadwayStream;
 } Decoder;
 
 extern void broadwayOnHeadersDecoded();
@@ -63,11 +65,26 @@ packetize: packet-by-packet mode
 cropDisplay: display cropped image (default decoded image)
 disableOutputReordering: disable DPB output reordering
 */
-u32 broadwayInit(Decoder *dec, int nalUnitStream, int packetize, int cropDisplay, u32 disableOutputReordering);
+u32 broadwayInit(Decoder *dec, int nalUnitStream, int packetize,
+				 int cropDisplay, u32 disableOutputReordering);
 u32 broadwayDecode(Decoder *dec);
 void playStream(Decoder *dec, Stream *stream);
-void broadwayExit(Decoder *dec);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void broadwayExit(Decoder *decoder);
+
+#ifdef __cplusplus
+}
+#endif
+// void broadwayExit(Decoder *dec);
 
 u32 broadwayGetMajorVersion();
 u32 broadwayGetMinorVersion();
-#endif // DECODER_H_
+
+#ifdef __cplusplus
+}
+#endif
+#endif	// DECODER_H_
